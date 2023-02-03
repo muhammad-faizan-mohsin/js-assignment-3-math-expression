514335bfc138a13f86a2ff40c79d33c3fe5963c9
faizan dhedhi <120604695+muhammad-faizan-mohsin@users.noreply.github.com>
2023-02-04 04:28:45 +0500

    not compeleted

---
+++ app.js
@@ -0,0 +1,197 @@
+// Assignment 3
+
+
+// Exercise no 1
+
+// Answer 1
+
+// var a = 3;
+// var b = 5;
+// var c = a+b;
+// document.write("Sum of " + a + " and " + b + " is " + c)
+
+// Answer 2
+
+// subtraction
+// var a = 3;
+// var b = 5;
+// var c = a-b;
+// document.write("Sum of " + a + " and " + b + " is " + c)
+
+// multiplication
+// var a = 3;
+// var b = 5;
+// var c = a-b;
+// document.write("Sum of " + a + " and " + b + " is " + c)
+
+// division
+// var a = 3;
+// var b = 5;
+// var c = a/b;
+// document.write("Sum of " + a + " and " + b + " is " + c)
+
+// modulus
+// var a = 3;
+// var b = 5;
+// var c = a%b; 
+// document.write("Sum of " + a + " and " + b + " is " + c)
+
+// answer 3
+
+// var num ;
+// document.write("Value after variable declaration is "+num )
+
+// var num = 5;
+// var num2 = 3;
+// var ans = num+num2
+// document.write(" initial value: "+num)
+
+
+// document.write(" Value after increment is: "+ ++num)
+
+
+// var num3=num + 7
+// document.write(" Value after addition is: "+ (num3))
+
+// document.write(" Value after addition is: "+ (--num3))
+
+// document.write(" Value after addition is: "+ (num3 % 3))
+
+// answer 4
+
+// var movie = 600;
+// var tickets = 5;
+// var result= movie * tickets
+// document.write("Total cost to buy "+ tickets + " tickets to a movie is " + result+"PKR")
+
+// answer 5
+
+// var tableNum = 4;
+// document.write("Table Of "+ tableNum + "<br />")
+
+// var i = 1;
+// var result = i * tableNum;
+// document.write(tableNum + " x " + i + " = " + result + "<br />")
+
+// var i = 2;
+// var result = i * tableNum;
+// document.write(tableNum + " x " + i + " = " + result + "<br />")
+
+
+// var i = 3;
+// var result = i * tableNum;
+// document.write(tableNum + " x " + i + " = " + result + "<br />")
+
+
+
+// var i = 4;
+// var result = i * tableNum;
+// document.write(tableNum + " x " + i + " = " + result + "<br />")
+
+
+// var i = 5;
+// var result = i * tableNum;
+// document.write(tableNum + " x " + i + " = " + result + "<br />")
+
+// var i = 6;
+// var result = i * tableNum;
+// document.write(tableNum + " x " + i + " = " + result + "<br />")
+
+// var i = 7;
+// var result = i * tableNum;
+// document.write(tableNum + " x " + i + " = " + result + "<br />")
+
+
+// var i =8;
+// var result = i * tableNum;
+// document.write(tableNum + " x " + i + " = " + result + "<br />")
+
+
+// var i = 9;
+// var result = i * tableNum;
+// document.write(tableNum + " x " + i + " = " + result + "<br />")
+
+// var i = 10;
+// var result = i * tableNum;
+// document.write(tableNum + " x " + i + " = " + result + "<br />")
+
+
+// Answer 6
+
+// var celsius = 25;
+// var fer = 77;
+// celsius =  (fer - 32) * 5/9;  
+// fer =  (celsius * 9/5) + 32;
+
+// document.write(celsius +"℃"+ " is " + fer +"°F" + "<br />")
+
+
+
+// var fer2 = 70;
+// var celsius2 = 21.111111111111111;
+
+
+
+// fer2 =  (celsius2 * 9/5) + 32;
+// celsius2 =  (fer2 - 32) * 5/9;    
+
+// document.write(fer2 +"°F" + " is " +celsius2 +"℃") 
+
+
+// Answer 7
+
+// var priceOfItem1 = 650;
+// var quantityOfItem1 = 3;
+// var priceOfItem2 = 100;
+// var quantityOfItem2 = 7;
+// var shippingCharges = 100;
+
+// var totalOfSub = priceOfItem1 *  quantityOfItem1 + priceOfItem2 * quantityOfItem2 + shippingCharges
+
+// document.write("<h1>Shopping Cart</h1>" + "<br />")
+// document.write("Price Of Item 1 is " + priceOfItem1 + "<br />" )
+// document.write("Quantity Of Item 1 is " + quantityOfItem1  + "<br />")
+// document.write("Price Of Item 2 is " + priceOfItem2  + "<br />")
+// document.write("Quantity Of Item 2 is " + quantityOfItem2  + "<br />")
+// document.write("Shipping Charges is " + shippingCharges  + "<br />" + "<br />")
+// document.write("Total Cost Of Your Order "+totalOfSub)
+
+// Answer 8
+
+// var totalMarks = 980;
+// var marksObtained = 804;
+// var percentage = marksObtained/totalMarks*100
+// document.write("Total Marks: "+ totalMarks+"<br/>"+ "Obtained Marks: "+ marksObtained+"<br/>"+"Percentage: " +  percentage+"%")
+
+// Answer 9
+// var usDollar = 104.80;
+// var saudiRiyal = 28;
+
+// var usTotal = usDollar * 10;
+// var srTotal = saudiRiyal * 25;
+// var total = usTotal + srTotal;
+// document.write("Total Currency in PKR:  " + total)
+
+
+// Answer 10
+// var exp = (5 + 10) / 2
+// console.log(exp) 
+
+
+// Answer 11
+// var currentYear = 2016;
+// var birthYear = 1992;
+// var yourAge = currentYear - birthYear
+// document.write("<h1>Age Calculator</h1>"+ "<br/>" +"Current Year: " + currentYear + "<br/>" + "Birth Year: " + birthYear + "<br/>" + "Your Age: "+ yourAge )
+
+
+
+
+// Answer 13
+
+var favoriteSnack = "Chocolate Chip";
+var currentAge = 15;
+var maxAge = 65;
+var perDay = 3
+
+var eatTotal =
